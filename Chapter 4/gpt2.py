ac9354dac8713a4866c5a922dc0be62c030e1c73
Krishna752006 <kkc070506@gmail.com>
2026-04-01 15:03:31 +0530

    Chapter 4 Complete

--- Chapter 4/gpt2.py
+++ Chapter 4/gpt2.py
@@ -10,10 +10,13 @@ GPT_CONFIG_124M = {
 
 import torch
 import torch.nn as nn
+import tiktoken
 import sys
 sys.path.insert(0, '../Chapter 3')
 from selfattn import MultiHeadAttention
     
+tokenizer = tiktoken.get_encoding("gpt2")
+
 class LayerNorm(nn.Module):
     def __init__(self, emb_dim):
         super().__init__()
@@ -83,9 +86,106 @@ class TransformerBlock(nn.Module):
 
         return x
 
-torch.manual_seed(123)
-x = torch.rand(2, 4, 768) #A
-block = TransformerBlock(GPT_CONFIG_124M)
-output = block(x)
-print("Input shape:", x.shape)
-print("Output shape:", output.shape)
+# torch.manual_seed(123)
+# x = torch.rand(2, 4, 768) #A
+# block = TransformerBlock(GPT_CONFIG_124M)
+# output = block(x)
+# print("Input shape:", x.shape)
+# print("Output shape:", output.shape)
+
+class GPTModel(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        
+        self.tok_emb = nn.Embedding(cfg["vocab_size"], cfg["emb_dim"])
+        self.pos_emb = nn.Embedding(cfg["context_length"], cfg["emb_dim"])
+        self.drop_emb = nn.Dropout(cfg["drop_rate"])
+        
+        self.trf_blocks = nn.Sequential(
+            *[TransformerBlock(cfg) for _ in range(cfg["n_layers"])]
+        )
+        
+        self.final_norm = LayerNorm(cfg["emb_dim"])
+        
+        self.out_head = nn.Linear(
+            cfg["emb_dim"],
+            cfg["vocab_size"],
+            bias=False
+        )
+
+    def forward(self, in_idx):
+        batch_size, seq_len = in_idx.shape
+
+        tok_embeds = self.tok_emb(in_idx)  # token embeddings
+        
+        pos_embeds = self.pos_emb(
+            torch.arange(seq_len, device=in_idx.device)
+        )  # positional embeddings
+        
+        x = tok_embeds + pos_embeds
+        x = self.drop_emb(x)
+        x = self.trf_blocks(x)
+        x = self.final_norm(x)
+        
+        logits = self.out_head(x)
+        
+        return logits
+
+batch = []
+
+txt1 = "Every effort moves you"
+txt2 = "Every day holds a"
+
+batch.append(torch.tensor(tokenizer.encode(txt1)))
+batch.append(torch.tensor(tokenizer.encode(txt2)))
+batch = torch.stack(batch, dim=0)
+
+# torch.manual_seed(123)
+model = GPTModel(GPT_CONFIG_124M)
+# out = model(batch)
+# # print("Input batch:\n", batch)
+# # print("\nOutput shape:", out.shape)
+# # print(out)
+# total_params = sum(p.numel() for p in model.parameters())
+# print(f"Total number of parameters: {total_params:,}")
+
+def generate_text_simple(model, idx, max_new_tokens, context_size):  # A
+    for _ in range(max_new_tokens):
+
+        # B: crop to last context_size tokens
+        idx_cond = idx[:, -context_size:]
+
+        with torch.no_grad():
+            logits = model(idx_cond)
+
+        # C: take logits of last time step
+        logits = logits[:, -1, :]
+
+        # D: convert to probabilities
+        probas = torch.softmax(logits, dim=-1)
+
+        # E: pick highest probability token
+        idx_next = torch.argmax(probas, dim=-1, keepdim=True)
+
+        # F: append predicted token
+        idx = torch.cat((idx, idx_next), dim=1)
+
+    return idx
+
+start_context = "Hello, I am"
+encoded = tokenizer.encode(start_context)
+# print("encoded:", encoded)
+encoded_tensor = torch.tensor(encoded).unsqueeze(0) #A
+# print("encoded_tensor.shape:", encoded_tensor.shape)
+
+model.eval() #A
+out = generate_text_simple(
+    model=model,
+    idx=encoded_tensor,
+    max_new_tokens=6,
+    context_size=GPT_CONFIG_124M["context_length"]
+)
+# print("Output:", out)
+# print("Output length:", len(out[0]))
+decoded_text = tokenizer.decode(out.squeeze(0).tolist())
+print(decoded_text)
